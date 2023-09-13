d722ed22f46fdea24bfb6d123a5de6e3e4f07f5f
eddbot <edwardsansome@gmail.com>
2023-09-13 22:55:59 +0100

    use normal_item to fit in with readme nomenclature

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,41 +8,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void generic_item_sell_in_decrements_by_one() {
-        Item[] items = new Item[]{new Item("generic_item", 0, 0)};
+    void normal_item_sell_in_decrements_by_one() {
+        Item[] items = new Item[]{new Item("normal_item", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("generic_item", app.items[0].name);
+        assertEquals("normal_item", app.items[0].name);
         assertEquals(0, app.items[0].quality);
         assertEquals(-1, app.items[0].sellIn);
     }
 
     @Test
-    void generic_item_quality_degrades_by_one_when_sell_in_not_reached() {
-        Item[] items = new Item[]{new Item("generic_item", 3, 10)};
+    void normal_item_quality_degrades_by_one_when_sell_in_not_reached() {
+        Item[] items = new Item[]{new Item("normal_item", 3, 10)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("generic_item", app.items[0].name);
+        assertEquals("normal_item", app.items[0].name);
         assertEquals(9, app.items[0].quality);
         assertEquals(2, app.items[0].sellIn);
     }
 
     @Test
-    void generic_item_quality_degrades_by_two_when_sell_in_reached() {
-        Item[] items = new Item[]{new Item("generic_item", 0, 10)};
+    void normal_item_quality_degrades_by_two_when_sell_in_reached() {
+        Item[] items = new Item[]{new Item("normal_item", 0, 10)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("generic_item", app.items[0].name);
+        assertEquals("normal_item", app.items[0].name);
         assertEquals(8, app.items[0].quality);
         assertEquals(-1, app.items[0].sellIn);
     }
 
     @Test
-    void generic_item_quality_cannot_be_negative() {
-        Item[] items = new Item[]{new Item("generic_item", 0, 0)};
+    void normal_item_quality_cannot_be_negative() {
+        Item[] items = new Item[]{new Item("normal_item", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("generic_item", app.items[0].name);
+        assertEquals("normal_item", app.items[0].name);
         assertEquals(0, app.items[0].quality);
         assertEquals(-1, app.items[0].sellIn);
     }
