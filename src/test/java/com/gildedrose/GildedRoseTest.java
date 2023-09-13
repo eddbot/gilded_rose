445ade82cb369a1b55b722956748e65b04e676b8
eddbot <edwardsansome@gmail.com>
2023-09-13 22:20:33 +0100

    test generic item behaviour

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,11 +8,31 @@ class GildedRoseTest {
 
     @Test
     void generic_item_sell_in_decrements_by_one() {
-        Item[] items = new Item[] { new Item("generic_item", 0, 0) };
+        Item[] items = new Item[]{new Item("generic_item", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("generic_item", app.items[0].name);
         assertEquals(0, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
+
+    @Test
+    void generic_item_quality_degrades_by_one_when_sell_in_not_reached() {
+        Item[] items = new Item[]{new Item("generic_item", 3, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("generic_item", app.items[0].name);
+        assertEquals(9, app.items[0].quality);
+        assertEquals(2, app.items[0].sellIn);
+    }
+
+    @Test
+    void generic_item_quality_degrades_by_two_when_sell_in_reached() {
+        Item[] items = new Item[]{new Item("generic_item", 0, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("generic_item", app.items[0].name);
+        assertEquals(8, app.items[0].quality);
         assertEquals(-1, app.items[0].sellIn);
     }
 
