2ae7846250dd72045ee12b63800d04bd24022639
eddbot <edwardsansome@gmail.com>
2023-09-14 08:53:30 +0100

    backstage pass item passing using new approach

--- src/main/java/com/gildedrose/BackstagePassItem.java
+++ src/main/java/com/gildedrose/BackstagePassItem.java
@@ -4,4 +4,36 @@ public class BackstagePassItem extends PubItem {
     public BackstagePassItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
+
+    @Override
+    public void update() {
+        updateQuality();
+        updateSellIn();
+
+    }
+
+    private void updateQuality() {
+
+        if (sellIn <= 0) {
+            quality = 0;
+            return;
+        }
+
+        int qualityToAdd = 0;
+
+        if (sellIn <= 10) {
+            qualityToAdd = 2;
+        }
+
+        if (sellIn <= 5) {
+            qualityToAdd = 3;
+        }
+
+        quality += qualityToAdd;
+    }
+
+    private void updateSellIn() {
+        sellIn -= 1;
+
+    }
 }
